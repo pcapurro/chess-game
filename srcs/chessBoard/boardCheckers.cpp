70e40f3c210a3c671adedba0e20261ee5b94773d
pcapurro <fossa634@gmail.com>
2024-10-20 16:28:11 +0200

    fixed wrong if statement in isDraw method

--- srcs/chessBoard/boardCheckers.cpp
+++ srcs/chessBoard/boardCheckers.cpp
@@ -50,10 +50,12 @@ bool    chessBoard::isDraw(void)
         }
         for (int i = 0; i != 64; i++)
         {
-            if (_board.at(kingPos).piece->isOnMyWay(_board.at(i).coord, boardCoords) == true
-                && _board.at(i).piece->getColor() != kingColor
-                && doesItResolveCheck(_board.at(kingPos).coord + _board.at(i).coord) == true)
-                return (false);
+            if (_board.at(kingPos).piece->isOnMyWay(_board.at(i).coord, boardCoords) == true)
+            {
+                if ((_board.at(i).piece == NULL || _board.at(i).piece->getColor() != kingColor)
+                    && doesItResolveCheck(_board.at(kingPos).coord + _board.at(i).coord) == true)
+                    return (false);
+            }
         }
         for (int i = 0; i != 64; i++)
         {
@@ -61,7 +63,7 @@ bool    chessBoard::isDraw(void)
             {
                 for (int k = 0; k != 64; k++)
                 {
-                    if (_board.at(i).piece->isOnMyWay(_board.at(i).coord + _board.at(k).coord, boardCoords) == true)
+                    if (_board.at(i).piece->isOnMyWay(_board.at(k).coord, boardCoords) == true)
                         return (false);
                 }
             }
