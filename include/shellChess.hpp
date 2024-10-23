4477f66c5956d186dd139896e8f63a9c4dce5384
pcapurro <fossa634@gmail.com>
2024-10-23 22:36:49 +0200

    added initializeGame function, added t_game and t_textures structures

--- include/shellChess.hpp
+++ include/shellChess.hpp
@@ -30,6 +30,33 @@
 
 using namespace std;
 
+typedef struct s_textures
+{
+    SDL_Texture     *king;
+    SDL_Texture     *queen;
+
+    SDL_Texture     *bishop;
+    SDL_Texture     *knight;
+    SDL_Texture     *rook;
+
+    SDL_Texture     *pawn;
+
+}   t_textures;
+
+typedef struct s_game
+{
+    SDL_Window      *mainWindow;
+    SDL_Renderer    *mainRenderer;
+
+    SDL_Texture     *boardTexture;
+    t_textures      whiteTextures;
+    t_textures      blackTextures;
+
+    bool            error;
+    bool            state;
+
+}   t_game;
+
 void    printTitle(void);
 void    initWelcome(void);
 
--- srcs/visual.cpp
+++ srcs/visual.cpp
@@ -1,35 +1,43 @@
 #include "../include/shellChess.hpp"
 
+void    initializeGame(t_game *mainStr)
+{
+    mainStr->mainWindow = SDL_CreateWindow("shell-chess", 100, 100, \
+        700, 700, SDL_WINDOW_SHOWN);
+
+    mainStr->mainRenderer = SDL_CreateRenderer(mainStr->mainWindow, -1, \
+        SDL_RENDERER_ACCELERATED);
+    
+    IMG_Init(IMG_INIT_PNG);
+}
+
 int visualGame(void)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-        return (systemFailed());
+    t_game  *mainStr;
+
+    mainStr = new (std::nothrow) t_game;
+    if (mainStr == nullptr)
+        return (1);
     else
     {
-        SDL_Window      *mainWindow;
-        SDL_Renderer    *mainRenderer;
-
-        mainWindow = SDL_CreateWindow("shell-chess", 100, 100, \
-                    700, 700, SDL_WINDOW_SHOWN);
-
-        mainRenderer = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED);
-
-        IMG_Init(IMG_INIT_PNG);
+        if (SDL_Init(SDL_INIT_VIDEO) != 0)
+            return (1);
+        initializeGame(mainStr);
 
         SDL_Surface *boardSurface;
         boardSurface = IMG_Load("./materials/board.png");
 
         SDL_Texture *boardTexture;
-        boardTexture = SDL_CreateTextureFromSurface(mainRenderer, boardSurface);
+        boardTexture = SDL_CreateTextureFromSurface(mainStr->mainRenderer, boardSurface);
         SDL_FreeSurface(boardSurface);
 
-        SDL_RenderClear(mainRenderer);
-        SDL_RenderCopy(mainRenderer, boardTexture, NULL, NULL);
-        SDL_RenderPresent(mainRenderer);
+        SDL_RenderClear(mainStr->mainRenderer);
+        SDL_RenderCopy(mainStr->mainRenderer, boardTexture, NULL, NULL);
+        SDL_RenderPresent(mainStr->mainRenderer);
 
         SDL_Delay(6000);
 
-        SDL_DestroyWindow(mainWindow);
+        SDL_DestroyWindow(mainStr->mainWindow);
         SDL_Quit();
     }
 
