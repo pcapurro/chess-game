ce44887456e4c1361cf35b4af810d8e779d76a58
pcapurro <fossa634@gmail.com>
2024-10-24 20:24:03 +0200

    added getRectangle method, adjusted loadBoard main loop

--- srcs/visual/visualGame/visualDisplay.cpp
+++ srcs/visual/visualGame/visualDisplay.cpp
@@ -37,7 +37,17 @@ SDL_Texture *VisualGame::getTexture(const char type, const string color) const
 
 SDL_Rect    VisualGame::getRectangle(const string coords)
 {
+    int         x;
+    int         y;
     SDL_Rect    obj;
+
+    x = coords[0] - 97;
+    y = atoi(coords.c_str() + 1);
+
+    obj.h = _height / 9, obj.w = _width / 9;
+    obj.x = _width / 10 + (_width / 10 * x), obj.y = _height / 10 + (_width / 10 * y);
+
+    return (obj);
 }
 
 void    VisualGame::loadBoard(const chessBoard *board)
@@ -45,11 +55,12 @@ void    VisualGame::loadBoard(const chessBoard *board)
     char        type;
     string      coords;
     string      color;
+    SDL_Rect    obj;
 
     SDL_RenderClear(_mainRenderer);
     SDL_RenderCopy(_mainRenderer, _boardTexture, NULL, NULL);
 
-    for (int i = 8; i != -1; i--)
+    for (int i = 9; i != 1; i--)
     {
         for (int k = 0; k != 8; k++)
         {
@@ -58,8 +69,11 @@ void    VisualGame::loadBoard(const chessBoard *board)
             color = board->getColor(coords);
 
             if (type != ' ')
+            {
+                obj = getRectangle(coords);
                 SDL_RenderCopy(_mainRenderer, getTexture(type, color), \
-                    NULL, &getRectangle(coords));
+                    NULL, &obj);
+            }
         }
     }
 }
