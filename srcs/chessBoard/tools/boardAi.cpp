5ed5065af23fca82d337baa732d30b8792aa4227
pcapurro <fossa634@gmail.com>
2024-11-08 20:33:36 +0100

    ai can now see if a victory is possible in one move

--- srcs/chessBoard/tools/boardAi.cpp
+++ srcs/chessBoard/tools/boardAi.cpp
@@ -2,21 +2,32 @@
 
 string  chessBoard::getAiNextMove(const int aiSide)
 {
-	if (isCheckMateNextMove(true) == true)
+	if (isCheckMateNextMove(true) == true) // defeat in 1 move
 		cout << "player can checkmate next move" << endl;
 	else
 		cout << "player can't checkmate next move" << endl;
+
+	++_turn % 2 == 0 ? _color = "white" : _color = "black";
+
+	if (isCheckMateNextMove(true) == true) // victory in 1 move
+		cout << "ai can win next move" << endl;
+	else
+		cout << "ai can't win next move" << endl;
+
+	--_turn % 2 == 0 ? _color = "white" : _color = "black";
 
 	static int i = 0;
 
 	i++;
 
 	if (i == 1)
-		return ("Pa7a5");
+		return ("Pe7e5");
 	if (i == 2)
-		return ("Pa5a4");
+		return ("Qd8f6");
 	if (i == 3)
-		return ("Pa4a3");
+		return ("Bf8c5");
+	if (i == 4)
+		return ("Qf6f2");
 
-	return ("Pe7e5");
+	return ("Qf6f2");
 }
