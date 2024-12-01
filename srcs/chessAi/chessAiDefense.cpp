83144ed6e9d0f97e408ffc9af1ecec9aed5777b6
pcapurro <fossa634@gmail.com>
2024-12-01 18:04:32 +0100

    added one more verification for a suggested protection move in getProtectAnswers method

--- srcs/chessAi/chessAiDefense.cpp
+++ srcs/chessAi/chessAiDefense.cpp
@@ -94,7 +94,7 @@ vector<string>  chessAi::getProtectAnswers(chessPiece *target)
         if (isMoveWorth(testMove) == true)
         {
             tryMove(testMove);
-            if (isAttacked(target->getCoord()) == false)
+            if (isAttacked(target->getCoord()) == false && isAttacked(string(1, testMove[2]) + testMove[3]) == false)
                 answers.push_back(legalMoves.at(i)), cout << legalMoves.at(i) << " can save it." << endl;
             undoMove(testMove);
         }
